c8b181745df48a39a6c6a3659ff0447443616fff
RaulAM <raulartilesm@gmail.com>
2024-11-26 16:05:13 +0000

    UPDATE: ROTO

---
+++ solucion-servir-html.js
@@ -0,0 +1,10 @@
+let express = require('express')
+let app  = express()
+
+app.get("/", (req, res) => {
+    const absolutePath = __dirname + "/views/index.html"
+    res.sendFile(absolutePath);
+})
+
+
+module.exports = app
