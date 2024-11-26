20b10c51f95d201fb028cc94f042b24e867c240a
RaulAM <raulartilesm@gmail.com>
2024-11-26 16:58:20 +0000

    update

--- solucion-servir-html.js
+++ solucion-servir-html.js
@@ -1,10 +1,40 @@
-let express = require('express')
-let app  = express()
+// Cargar el .env
+require('dotenv').config();
 
+// Dependencias
+let express = require('express');
+let app = express();
+
+//Debugger
+console.log("Hello World")
+
+// Middlewares
+
+// Hojas de estilos
+app.use("/public", express.static(__dirname + "/public"));
+
+// Logger
+
+
+// Rutas
+// Ruta de Home
 app.get("/", (req, res) => {
     const absolutePath = __dirname + "/views/index.html"
-    res.sendFile(absolutePath);
+    res.sendFile(absolutePath)
+})
+
+// Ruta de /Json
+app.get("/json", (req, res) => {
+    let responseMessage = "Hello json";
+
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        responseMessage = responseMessage.toUpperCase();
+    }
+    res.json({"message": responseMessage})
 })
 
 
-module.exports = app
+
+
+
+ module.exports = app;
