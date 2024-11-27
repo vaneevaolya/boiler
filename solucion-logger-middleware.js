1636ac447c2c7b190c0b36b908389eda169c02cd
RaulAM <raulartilesm@gmail.com>
2024-11-27 14:05:13 +0000

    update middleware chaining

--- solucion-logger-middleware.js
+++ solucion-logger-middleware.js
@@ -15,11 +15,24 @@ app.use("/public", express.static(__dirname + "/public"));
 // Logger
 app.use((req, res, next) => {
     const log = `${req.method} ${req.path} - ${req.ip}`;
-
     console.log(log)
-
     next()
 })
+
+
+// Middleware chaining on routes
+// Ruta /now con middleware encadenado
+app.get("/now", (req, res, next) => {
+    
+    // Middleware para agregar la hora actual al objeto req
+    req.time = new Date().toString()
+    next()
+
+    // Controlador final
+}, (req, res) => {
+    res.json({time: req.time})
+})
+
 
 // Rutas
 // Ruta de Home
@@ -31,10 +44,8 @@ app.get("/", (req, res) => {
 // Ruta de /Json
 app.get("/json", (req, res) => {
     let responseMessage = "Hello json";
-
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        responseMessage = responseMessage.toUpperCase();
-    }
+        responseMessage = responseMessage.toUpperCase();}
     res.json({ "message": responseMessage })
 })
 
