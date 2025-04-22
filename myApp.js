5a1de4b3669d3cd1a6146189bfaffd0b55d57171
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-04-22 22:50:22 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 
 app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 
