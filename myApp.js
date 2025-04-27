545cae086831b36826f9b7920984efdd9e814847
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-04-27 21:36:05 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", (req, res) => {
 if (process.env.MESSAGE_STYLE === "uppercase") {
-	res.json({"message": "Hello json".toUpperCase});
+	return res.json({"message": "Hello json".toUpperCase});
 } else {
   res.json({"message": "Hello json"});
 }
