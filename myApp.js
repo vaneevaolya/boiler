ff113e88235008aac4a4d5f542c2994b0a916bcb
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-04-27 19:18:02 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,8 @@ let app = express();
 
 
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", res.json({"message": "Hello json"}))
+app.get("/json", function(req, res) {
+  res.json({"message": "Hello json"})});
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
