15f07cdf5591810a1652bf97318ebbd350679db9
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-04-27 21:01:52 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,16 @@
 let express = require("express");
 let app = express();
+require('dotenv').config()
 
 
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/json", function(req, res) {
-  res.json({"message": "Hello json"})});
+  res.json(
+    if (process.env.VAR_NAME === "uppercase") {
+  response = "Hello World".toUpperCase();
+} else {
+  response = "Hello World";
+});
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
