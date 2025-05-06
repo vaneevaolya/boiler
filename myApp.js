23533dbdf28e98e9ad23e4f053df5db1388fb368
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-05-06 23:12:06 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,12 +1,18 @@
-let express = require("express");
-let app = express();
+const express = require('express');
+const app = express();
 
-
-app.use('/', (req, res, next)=>{
-console.log("req.method+ ’ ‘+req.path+’-’+req.ip ");
-next();
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + '-' + req.ip);
+  next();
 });
 
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+
+app.listen(3000, () => {
+  console.log('listening');
+});
 
 
 
