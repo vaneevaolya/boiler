bd72f54cdf9722e30ad8a9d6fb08aa3f968b4b6c
Olga Vaneeva <146543384+vaneevaolya@users.noreply.github.com>
2025-05-06 23:15:14 +0200

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log(req.method + ' ' + req.path + '-' + req.ip);
   next();
 });
